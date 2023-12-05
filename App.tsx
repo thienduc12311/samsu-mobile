028c27a6e7a79774a024c0090463b659c3ad29b1
Duc Nguyen <ducn@intelisysaviation.com>
2023-12-06 01:16:12 +0700

    implement firebase for react native

--- App.tsx
+++ App.tsx
@@ -1,27 +1,53 @@
+import messaging, { firebase } from '@react-native-firebase/messaging'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import React, { useCallback, useEffect } from 'react'
-import { LogBox } from 'react-native'
+import { Alert, LogBox, Permission, PermissionsAndroid } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { FONTS } from './constants/fonts'
 import { AppContextProvider } from './contexts/AppContext'
 import AppNavigation from './navigations/AppNavigation'
-// async function requestUserPermission() {
-//     const authStatus = await messaging().requestPermission();
-//     PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS as Permission);
-//     const enabled =
-//         authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-//         authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS as Permission);
+async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-//     if (enabled) {
-//         console.log('Authorization status:', authStatus);
-//     }
-// }
+    if (enabled) {
+        console.log('Authorization status:', authStatus);
+    }
+}
+const getToken = async () => {
+    try {
+        const token = await firebase.messaging().getToken();
+        if (token) return token;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getFCMToken = async () => {
+    try {
+        const authorized = await firebase.messaging().hasPermission();
+        const fcmToken = await getToken();
+        console.log(fcmToken);
+        if (authorized) return fcmToken;
+
+        await firebase.messaging().requestPermission();
+        return fcmToken;
+    } catch (error) {
+        console.log(error);
+    }
+};
 //Ignore all log notifications
 LogBox.ignoreAllLogs()
 
 SplashScreen.preventAutoHideAsync()
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
 
 export default function App() {
     const [fontsLoaded] = useFonts(FONTS)
@@ -29,8 +55,16 @@ export default function App() {
         const getBarCodeScannerPermissions = async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
         };
-        // requestUserPermission();
-        getBarCodeScannerPermissions();
+        requestUserPermission();
+        getBarCodeScannerPermissions(); 
+        getFCMToken();
+    }, []);
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert((remoteMessage.notification?.title as string), (remoteMessage.notification?.body));
+        });
+
+        return unsubscribe;
     }, []);
     const onLayoutRootView = useCallback(async () => {
         if (fontsLoaded) {
@@ -43,7 +77,7 @@ export default function App() {
     }
 
     return (
-        <AppContextProvider children={undefined}>
+        <AppContextProvider>
             <SafeAreaProvider onLayout={onLayoutRootView}>
                 <AppNavigation />
             </SafeAreaProvider>
